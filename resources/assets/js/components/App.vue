91a34971ea5fbbdb27dc00f44afcb7f13a6c6bc9
Anthony Gore <anthony@magnum.technology>
2018-02-02 13:36:58 +0700

    First commit

---
+++ resources/assets/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
